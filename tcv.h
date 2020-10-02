68fdee372bdc3b0c9cc4bb02ebe2395a9e8a49c9
fethiuqam <abi_ayad.fethi_bey@courrier.uqam.ca>
2020-10-02 17:38:11 -0400

    creation du fichier tp1.c avec le template du max_test.c, et ajout du .gitignore et les fichiers tcv.h et tcv.o

---
+++ tcv.h
@@ -0,0 +1,22 @@
+#ifndef __TCV_H__
+#define __TCV_H__
+
+#include <stdbool.h>
+
+bool validerTH_1(int);
+bool validerTH_2(char);
+bool validerTH_3(short);
+
+bool validerTA_1(int);
+bool validerTA_2(char);
+bool validerTA_3(short);
+
+bool validerPulsation_1(int);
+bool validerPulsation_2(char);
+bool validerPulsation_3(short);
+
+bool validerSignal_1(int);
+bool validerSignal_2(char);
+bool validerSignal_3(short);
+
+#endif
